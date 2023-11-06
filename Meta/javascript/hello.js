7d39538fc2ce440216b372807375894a3fccecf3
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-06 15:45:19 +0100

    Deleted a file that contained info about my meta workspace and currently no adverse effect. Wrote more javascript introduction notes

--- Meta/javascript/hello.js
+++ Meta/javascript/hello.js
@@ -38,7 +38,21 @@ var ugly = false; // This is a boolean
 Null means the absence of a value 
 Undefined means a variable has not been assigned a value
 */
+// Mathematical operators
 console.log(2 + 2); // addition operation outputs: 4
 console.log(2 - 2); // subtraction operation outputs: 0
 console.log(2 / 2); // division operation outputs: 1
-console.log(2 * 2); // multiplcation outputs: 4
+console.log(2 * 2); // multiplcation operation outputs: 4
+console.log(2 ** 2); // raised to power operation outputs: 4
+console.log(7 % 2); // modulus operation outputs: 1
+// comparison operators
+console.log(2 > 3); // greter than operation outputs: false
+console.log(2 < 3); // less than operation outputs: true
+console.log(2 == 3); // equality operation outputs: false. It checks for only the value.
+console.log(7 === "7"); // The strict equality operator checks for both value and type. Outputs false.
+console.log(2 != "2"); // The inequality operation outputs: false. checks for only value
+console.log(2 !== "2"); //The strict inequality operation outputs: true. checks for both value and type
+// Logical operators
+console.log(true || false); // The or operator outputs: true
+console.log(true && false); // The and operator outputs: false
+console.log(! true); // The not operator outputs: false
