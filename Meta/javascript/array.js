b4e23b4a97a445e961b8934af836fe62a9147500
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-13 18:02:25 +0100

    Started working with arrays, created functions lengthOfArray, range, sum.

---
+++ Meta/javascript/array.js
@@ -0,0 +1,7 @@
+var emptyArray = [];
+
+var phoneArray = ["Apple", "Samsung", "Tecno", "Infinix", "Huawei"];
+
+for (var x = 0; x < phoneArray.length; x++) {
+  console.log(phoneArray[x]);
+}
--- Meta/javascript/function.js
+++ Meta/javascript/function.js
@@ -9,3 +9,43 @@ function checkSalary(age) {
 }
 
 console.log(checkSalary(30));
+
+function lengthOfArray(array) {
+  var x = 0;
+  var y = 1;
+  while (x < y) {
+    if (array[0] == undefined) {
+      return 0;
+    }
+    if (array[y] == undefined && array[x] != undefined) {
+      return y;
+    } else if (array[y] == undefined && array[x] != undefined) {
+      x -= 1;
+      y -= 1;
+    } else if (array[y] != undefined && array[x] != undefined) {
+      x += 1;
+      y += 1;
+    }
+  }
+}
+
+console.log(lengthOfArray(["david", "boy", "girl", "akachukwu", "beautiful"]));
+
+function range(start = 0, end, step = 1) {
+  var newArray = [];
+  for (var x = start; x < end; x += step) {
+    newArray = newArray.concat([x]);
+  }
+  return newArray;
+}
+
+console.log(range(0, 5, 2));
+
+function sum(array) {
+  var sum = 0;
+  for (var x = 0; x < array.length; x++) {
+    sum += array[x];
+  }
+  return sum;
+}
+console.log(sum(range(1, 5)));
