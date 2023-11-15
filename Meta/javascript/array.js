ef0013f574d68740a61bddfafab3a59af573013e
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-15 17:17:18 +0100

    Added more array methods

--- Meta/javascript/array.js
+++ Meta/javascript/array.js
@@ -5,3 +5,8 @@ var phoneArray = ["Apple", "Samsung", "Tecno", "Infinix", "Huawei"];
 for (var x = 0; x < phoneArray.length; x++) {
   console.log(phoneArray[x]);
 }
+
+var pcArray = ["Dell", "Lenovo", "HP", "Apple"];
+var gadgetArray = phoneArray.concat(pcArray);
+gadgetArray.pop();
+console.log(gadgetArray);
