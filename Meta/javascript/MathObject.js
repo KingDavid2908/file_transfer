f6cbdb1083aac195cf498862cd83cb1055447ef8
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-15 17:18:27 +0100

    I created a file to store information about common math objects

---
+++ Meta/javascript/MathObject.js
@@ -0,0 +1,18 @@
+console.log(Math.PI); // Maths constant for pie
+console.log(Math.E); // Maths constant for euler constant
+console.log(Math.LN2); // Maths constant for the natural logarithm of 2
+console.log(Math.ceil(23.56)); // Maths method to round up to the nearest whole number
+console.log(Math.floor(23.56)); // Maths method to round down to the nearest whole number
+console.log(Math.round(23.56)); // Maths method to round to the nearest whole number
+console.log(Math.round(23.46));
+console.log(Math.trunc(23.56)); // Maths method to trim the decimal leaving only the integers
+console.log(Math.pow(2, 5));
+console.log(Math.sqrt(36));
+console.log(Math.cbrt(216));
+console.log(Math.abs(-23));
+console.log(Math.log(4));
+console.log(Math.log2(4));
+console.log(Math.log10(4));
+console.log(Math.min(1, 23, 92));
+console.log(Math.max(1, 23, 92));
+console.log(Math.sin(10), Math.cos(10), Math.tan(10));
