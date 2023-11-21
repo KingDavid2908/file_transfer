01eea27880442c700b26b5052d0749325568790d
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-21 15:00:35 +0100

    I created a file string.js to practice some inbuilt methods for working with strings.

---
+++ Meta/javascript/string.js
@@ -0,0 +1,8 @@
+var namePhrase = "My name is david.";
+console.log(namePhrase.charAt(3));
+console.log(namePhrase.indexOf("i"));
+console.log(namePhrase.lastIndexOf("i"));
+phraseArray = namePhrase.split(" ");
+console.log(phraseArray);
+console.log(namePhrase.toLowerCase());
+console.log(namePhrase.toUpperCase());
