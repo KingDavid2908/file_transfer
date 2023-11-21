8adeffe80e23656277ca1e1c8f4bce7bec9af644
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-21 14:57:42 +0100

    I created functions to work with arrays and used some inbuilt functions.

--- Meta/javascript/array.js
+++ Meta/javascript/array.js
@@ -8,5 +8,22 @@ for (var x = 0; x < phoneArray.length; x++) {
 
 var pcArray = ["Dell", "Lenovo", "HP", "Apple"];
 var gadgetArray = phoneArray.concat(pcArray);
-gadgetArray.pop();
+console.log(gadgetArray.pop());
 console.log(gadgetArray);
+
+function convertArrayToString(array) {
+  var string = "";
+  var length = array.length;
+  for (var i = 0; i < length; i++) {
+    if (i == length - 1) {
+      var newString = array[i] + ".";
+      string = string.concat(newString);
+    } else {
+      var newString = array[i] + ", ";
+      string = string.concat(newString);
+    }
+  }
+  return string;
+}
+
+console.log("The array consists of", convertArrayToString(gadgetArray));
