6feb9c6621a64fa4a1f1e1986c2335f0e702cc78
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-11-22 19:37:51 +0100

    Function that adds two numbers together.

--- Meta/javascript/sumTwoNum.js
+++ Meta/javascript/sumTwoNum.js
@@ -1,12 +1,18 @@
-function sumTwoNums(x, y) {
-  datatype = typeof x;
-  if (typeof y == datatype) {
-    return x + y;
-  } else {
-    return "The inputs should only be numbers";
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != "number") {
+      throw new ReferenceError("the first argument is not a number");
+    } else if (typeof b != "number") {
+      throw new ReferenceError("the second argument is not a number");
+    } else {
+      return a + b;
+    }
+  } catch (err) {
+    return "Error!", err;
   }
 }
+addTwoNums(5, 5);
 
-console.log(sumTwoNums(2, 3));
-console.log(sumTwoNums(2, "3"));
-console.log(sumTwoNums("2", "3"));
+console.log(addTwoNums(2, 3));
+console.log(addTwoNums(2, "3"));
+console.log(addTwoNums("2", "3"));
