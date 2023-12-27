14918a2ea70e96046ab2381d3285a8750e17dfe8
Chukwumam Akachukwu <akachukwumam1@gmail.com>
2023-12-27 21:59:08 +0100

    Learnt more on classes

--- Meta/javascript/class.js
+++ Meta/javascript/class.js
@@ -20,18 +20,18 @@ ringTheBell(door); // "Ring, ring! Come here, please!"
 ["abc"].concat(["def"]); // ['abc', 'def']
 ["abc"] + ["def"]; // ["abcdef"]
 
-class Bird {
+class Birdy {
   useWings() {
     console.log("Flying!");
   }
 }
-class Eagle extends Bird {
+class Eagle extends Birdy {
   useWings() {
     super.useWings();
     console.log("Barely flapping!");
   }
 }
-class Penguin extends Bird {
+class Penguin extends Birdy {
   useWings() {
     console.log("Diving!");
   }
@@ -41,7 +41,7 @@ var kingPenguin = new Penguin();
 baldEagle.useWings(); // "Flying! Barely flapping!"
 kingPenguin.useWings(); // "Diving!"
 
-class Animal {
+class Animals {
   constructor(classification, sound, legs) {
     this.classification = classification;
     this.sound = sound;
@@ -70,8 +70,8 @@ class Animal {
   }
 }
 
-class Dog extends Animal {
-  constructor(firstName) {
+class Dog extends Animals {
+  constructor(firstName = "Bingo") {
     super();
     this.classification = "Dog";
     this.sound = "Bark";
@@ -81,7 +81,7 @@ class Dog extends Animal {
   }
 }
 
-class Birds extends Animal {
+class Birdys extends Animals {
   constructor(classification, sound, firstName) {
     super(classification, sound);
     this.legs = 2;
@@ -89,9 +89,9 @@ class Birds extends Animal {
   }
 }
 
-let cat = new Animal("Cat", "meow", 4);
+let cat = new Animals("Cat", "meow", 4);
 let bingo = new Dog("Bingo");
-let john = new Birds("Parrot", "talk", "John");
+let john = new Birdys("Parrot", "talk", "John");
 
 bingo.sayName();
 bingo.sayAge();
@@ -147,7 +147,7 @@ class Train {
 }
 
 class HighSpeedTrain extends Train {
-  constructor(passengers, highSpeedOn, color, lightsOn) {
+  constructor(passengers = 100, highSpeedOn = true, color, lightsOn) {
     super(color, lightsOn);
     this.passengers = passengers;
     this.highSpeedOn = highSpeedOn;
@@ -201,7 +201,11 @@ class Treadmill {
 }
 
 class Gym {
-  constructor(openHrs, stationaryBikePos, treadmillPos) {
+  constructor(
+    openHrs = "6-20",
+    stationaryBikePos = "left corner",
+    treadmillPos = "right corner"
+  ) {
     this.openHrs = openHrs;
     this.stationaryBike = new StationaryBike(stationaryBikePos, 8);
     this.treadmill = new Treadmill(treadmillPos, 5);
@@ -209,7 +213,149 @@ class Gym {
 }
 
 var boxingGym = new Gym("7-22", "right corner", "left corner");
+var wrestlinGym = new Gym();
 
+console.log(wrestlinGym.stationaryBike);
 console.log(boxingGym.openHrs); //
 console.log(boxingGym.stationaryBike); //
 console.log(boxingGym.treadmill); //
+
+class Animal {
+  constructor(color = "yellow", energy = 100) {
+    this.color = color;
+    this.energy = energy;
+  }
+  isActive() {
+    if (this.energy > 0) {
+      this.energy -= 20;
+      console.log("Energy is decreasing, currently at:", this.energy);
+    } else if (this.energy == 0) {
+      this.sleep();
+    }
+  }
+  sleep() {
+    this.energy += 20;
+    console.log("Energy is increasing, currently at:", this.energy);
+  }
+  getColor() {
+    console.log(this.color);
+  }
+}
+
+class Cat extends Animal {
+  constructor(
+    sound = "purr",
+    canJumpHigh = true,
+    canClimbTrees = true,
+    color,
+    energy
+  ) {
+    super(color, energy);
+    this.sound = sound;
+    this.canClimbTrees = canClimbTrees;
+    this.canJumpHigh = canJumpHigh;
+  }
+  makeSound() {
+    console.log(this.sound);
+  }
+}
+
+class Bird extends Animal {
+  constructor(sound = "chirp", canFly = true, color, energy) {
+    super(color, energy);
+    this.sound = sound;
+    this.canFly = canFly;
+  }
+  makeSound() {
+    console.log(this.sound);
+  }
+}
+
+class HouseCat extends Cat {
+  constructor(
+    houseCatSound = "meow",
+    sound,
+    canJumpHigh,
+    canClimbTrees,
+    color,
+    energy
+  ) {
+    super(sound, canJumpHigh, canClimbTrees, color, energy);
+    this.houseCatSound = houseCatSound;
+  }
+  makeSound(option) {
+    if (option) {
+      super.makeSound();
+    }
+    console.log(this.houseCatSound);
+  }
+}
+
+class Tiger extends Cat {
+  constructor(
+    tigerSound = "Roar!",
+    sound,
+    canJumpHigh,
+    canClimbTrees,
+    color,
+    energy
+  ) {
+    super(sound, canJumpHigh, canClimbTrees, color, energy);
+    this.tigerSound = tigerSound;
+  }
+  makeSound(option) {
+    if (option) {
+      super.makeSound();
+    }
+    console.log(this.tigerSound);
+  }
+}
+
+class Parrot extends Bird {
+  constructor(canTalk = false, sound, canFly, color, energy) {
+    super(sound, canFly, color, energy);
+    this.canTalk = canTalk;
+  }
+  makeSound(option) {
+    if (option) {
+      super.makeSound();
+    }
+    if (this.canTalk) {
+      console.log("I'm a talking parrot!");
+    }
+  }
+}
+
+var fiji = new Parrot(false); // we're passing `false` to the constructor so that fiji can't talk
+var polly = new Parrot(true); // we're passing `true` to the constructor so that polly can talk
+
+fiji.makeSound(); // undefined
+fiji.makeSound(true); // chirp
+
+polly.makeSound(); // I'm a talking parrot!
+polly.makeSound(true); // chirp, I'm a talking parrot!
+
+polly.color; // yellow
+polly.energy; // 100
+
+polly.isActive(); // Energy is decreasing, currently at: 80
+
+var penguin = new Bird("shriek", false, "black and white", 200); // setting all the custom properties
+penguin; // Bird {color: 'black and white', energy: 200, sound: 'shriek', canFly: false }
+
+penguin.sound; // 'shriek'
+penguin.canFly; // false
+penguin.color; // 'black and white'
+penguin.energy; // 200
+penguin.isActive(); // Energy is decreasing, currently at: 180
+
+var leo = new HouseCat();
+
+// leo, no purring please:
+leo.makeSound(false); // meow
+// leo, both purr and meow now:
+leo.makeSound(true); // purr, meow
+
+var cuddles = new Tiger();
+cuddles.makeSound(false); // Roar!
+cuddles.makeSound(true); // purr, Roar!
